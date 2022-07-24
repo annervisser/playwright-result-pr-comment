b5391b1b7b5c8e356d171f2744d552b74b60a7dd
Anner Visser <annervisser@gmail.com>
2022-07-24 14:32:07 +0200

    Added playwright and ncc

---
+++ playwright/playwright.config.js
@@ -0,0 +1,33 @@
+// @ts-check
+const {devices} = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('@playwright/test').PlaywrightTestConfig}
+ */
+const config = {
+	testDir: './tests',
+	reporter: 'json',
+	use: {
+		trace: 'on-first-retry',
+	},
+
+	/* Configure projects for major browsers */
+	projects: [
+		{
+			name: 'chromium',
+			use: {
+				...devices['Desktop Chrome'],
+			},
+		}
+	]
+};
+
+module.exports = config;
